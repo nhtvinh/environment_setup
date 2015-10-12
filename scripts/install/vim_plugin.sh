0c2fcc05a8f00e18559682f66d2020f8b1a0391c
Vinh Nguyen <nhtvinh@yahoo.com>
2015-10-12 19:18:11 +0700

    add vim plugin install script

---
+++ scripts/install/vim_plugin.sh
@@ -0,0 +1,9 @@
+# Install Nerdtree
+cd $HOME/.vim/bundle/
+git clone https://github.com/scrooloose/nerdtree.git
+# Install tagbar
+git clone git://github.com/majutsushi/tagbar
+# Install vim-colors-solarized
+git clone git://github.com/altercation/vim-colors-solarized.git
+# Clone YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe.git
