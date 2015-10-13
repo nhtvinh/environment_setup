ef63267d8b9e7fc2fda18ffeb211bc3aad627da4
Vinh Nguyen <nhtvinh@yahoo.com>
2015-10-13 12:04:23 +0700

    update add Tmux plugin manager

--- scripts/install/03_tmux_2.0.sh
+++ scripts/install/03_tmux_2.0.sh
@@ -1,5 +1,11 @@
+# Install tmux 2.0 from source
 cd $HOME/environment_setup/tools/tmux-2.0
 sudo apt-get install libevent-dev
 ./configure prefix=$HOME/.local
 make
 make install
+
+# Install tpm (Tmux plugin manager)
+cd $HOME/.tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm
+
