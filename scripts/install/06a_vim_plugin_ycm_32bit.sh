5106f6c4f96a5146b26f13d39d2515c995085897
Vinh Nguyen <nhtvinh@yahoo.com>
2015-10-13 11:43:56 +0700

    updat script fix append test to /etc/apt/sources.list

--- scripts/install/06a_vim_plugin_ycm_32bit.sh
+++ scripts/install/06a_vim_plugin_ycm_32bit.sh
@@ -2,10 +2,9 @@
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo echo "# 3.7" >> /etc/apt/sources.list
-sudo echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main" >> /etc/apt/sources.list
-sudo echo "deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main" >> /etc/apt/sources.list
-
+sudo sed -i '$ a\## LLVM 3.7' /etc/apt/sources.list
+sudo sed -i '$ a\deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main' /etc/apt/sources.list
+sudo sed -i '$ a\deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install clang-3.7 lldb-3.7 libclang-3.7-dev
 cd ~/.vim/bundle/YouCompleteMe
